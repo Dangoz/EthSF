1638dbe2807de95eab382bce9adb119c7f7ab6ae
Mason Brothers <brothers.mason@gmail.com>
2024-10-19 15:39:01 -0600

    Merge in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Providers from "./providers";
+import NavBar from "@/components/common/NavBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <div className="w-screen h-fit min-h-[calc(100dvh)]">
           <Providers>
+            <NavBar />
             {children}
           </Providers>
         </div>
