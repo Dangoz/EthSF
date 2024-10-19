7598b018a613449bb3a7d7888d7617e9f671da0b
Mason Brothers <brothers.mason@gmail.com>
2024-10-19 15:59:32 -0600

    Adding discover stuff

--- src/components/common/ReviewCard.tsx
+++ src/components/common/ReviewCard.tsx
@@ -9,29 +9,24 @@ import {
 import { Heart, ThumbsDown } from "lucide-react"
 
 
-export const ReviewCard = (props: {title: string, description: string}) => <Card>
+export const ReviewCard = (props: {title: string, description: string, imageUrl: string, onLike: () => void, onDislike: () => void }) => <Card>
   <CardHeader>
     <CardTitle>{props.title}</CardTitle>
     <CardDescription>{props.description}</CardDescription>
   </CardHeader>
   <CardContent>
-    <>
-      <img
-        src="https://via.placeholder.com/300"
-        alt="Card Image"
-        className="w-full h-48 object-cover"
-      />
-      <p>
-
-      </p>
-    </>
+    <img
+      src="https://via.placeholder.com/300"
+      alt={props.title}
+      className="w-full h-48 object-cover"
+    />
   </CardContent>
   <CardFooter className="flex justify-around p-4">
     <>
-      <div className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-200">
+      <div onClick={props.onDislike} className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-200">
         <ThumbsDown className="w-6 h-6 text-gray-700" />
       </div>
-      <div className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-200">
+      <div onClick={props.onLike} className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-200">
         <Heart className="w-6 h-6 text-red-500" />
       </div>
     </>
