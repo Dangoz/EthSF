5b3bc5143eaf7cb9e3dbd0e83835c9008f6894ff
Dango <jason543211@hotmail.com>
2024-10-20 00:51:08 -0700

    add-review

--- src/app/api/add-review/route.ts
+++ src/app/api/add-review/route.ts
@@ -1,21 +1,12 @@
 import { uploadImageToIPFS } from "@/lib/pinata"
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import { addReview } from "@/model/review";
 
 export const POST = async (req: NextRequest) => {
   try {
-    // assuming image of type file is present in request
-
-    // TODO Get Reviews here
-
-
-    /*    pri
-        req.body
-    
-    
-        return NextResponse.json()
-        */
-
+    const { title, description, license, ipfsUrl, ipAssetId } = await req.json()
+    const review = await addReview({ title, description, license: `${license}`, ipfsUrl, ipAssetId })
+    return NextResponse.json({ review }, { status: 200 })
   } catch (error) {
     console.log(error)
     return NextResponse.json({ error: "Failed to upload media to IPFS" }, { status: 500 })
--- src/model/review.ts
+++ src/model/review.ts
@@ -1,7 +1,22 @@
 import prisma from "./prisma";
 
-export const addReview = async () => {
-  // const newReview = await prisma.review.create({
-  //     data: review
-  // })
+export const addReview = async ({
+  title,
+  description,
+  license,
+  ipfsUrl,
+  ipAssetId
+}: {
+  title: string, description: string, license: string, ipfsUrl: string, ipAssetId: string
+}) => {
+  const review = await prisma.iPA.create({
+    data: {
+      ipAssetId,
+      title,
+      description,
+      license,
+      ipfsUrl,
+    }
+  })
+  return review
 }
